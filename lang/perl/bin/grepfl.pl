52dfca1efb40e2f3e290d39ca82a30b4cae6957f
usr1 <usr1@lmint19.3>
2021-03-16 22:33:57 -0500

    asdfasdf

--- lang/perl/bin/grepfl.pl
+++ lang/perl/bin/grepfl.pl
@@ -21,16 +21,18 @@ use lib "$FindBin::RealBin/../lib";
 use My::Utils qw(
    add_new_lines
    get_cmd_line_options
+   get_recursive_list_of_dirs
    get_recursive_list_of_files
    grep_strs_in_array
    nvl
    verify_dir_is_readable
 );
 
-
 my @opt_spec = qw(
    ignore_case|i
    file_pattern|fp=s@
+   dirs|d
+   files|f
 );
 
 #--------------------------------------------------
@@ -47,21 +49,38 @@ my @strings = @ARGV;
 # process options
 my $ignore_case = nvl( $opts{ ignore_case }, 0 );
 my @file_patterns = @{ nvl( $opts{ file_pattern }, [ "*" ] ) };
+my $incl_dirs  = nvl( $opts{ dirs }, 0 );
+my $incl_files = nvl( $opts{ files }, 0 );
 
 # validate dir
 verify_dir_is_readable( $dir ) or confess "ERROR. dir not readable. dir=[$dir]";
 
-# get file list and scan for strings
-my @file_list = get_recursive_list_of_files(
-   dir      => $dir,
-   patterns => \@file_patterns,
-);
+# get filesys list and scan for strings
+my @filesys_list = ();
+if ( $incl_files )
+{
+   my @tmp = get_recursive_list_of_files(
+      dir      => $dir,
+      patterns => \@file_patterns,
+   );
+   push( @filesys_list, @tmp );
+}
+if ( $incl_dirs )
+{
+   my @tmp = get_recursive_list_of_dirs(
+      dir      => $dir,
+      patterns => \@file_patterns,
+   );
+   push( @filesys_list, @tmp );
+}
+
 my @matched_files = grep_strs_in_array(
    strs        => \@strings,
-   array       => \@file_list,
+   array       => \@filesys_list,
    ignore_case => $ignore_case,
 );
 
+# print matched items
 print "\n";
 print add_new_lines( @matched_files );
 
